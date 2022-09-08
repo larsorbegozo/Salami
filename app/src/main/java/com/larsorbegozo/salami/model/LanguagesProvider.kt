25ecfa6cb32bc78c1c0224eab31b5bf176aa943d
WezenKerrash <wezenkerrash44@gmail.com>
2022-09-08 11:48:58 -0300

    Logo changed

--- app/src/main/java/com/larsorbegozo/salami/model/LanguagesProvider.kt
+++ app/src/main/java/com/larsorbegozo/salami/model/LanguagesProvider.kt
@@ -3,12 +3,12 @@ package com.larsorbegozo.salami.model
 class LanguagesProvider {
     companion object {
         val languagesList: List<Languages> = listOf(
-            Languages("Inglés", "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/640px-Flag_of_the_United_Kingdom.svg.png"),
-            Languages("Francés", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1200px-Flag_of_France.svg.png"),
-            Languages("Alemán", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/320px-Flag_of_Germany.svg.png"),
-            Languages("Japonés", "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/2560px-Flag_of_Japan.svg.png"),
-            Languages("Italiano", "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/2560px-Flag_of_Italy.svg.png"),
-            Languages("Portugués", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1280px-Flag_of_Portugal.svg.png"),
+            Languages("Inglés", "https://i.imgur.com/BOy4RJ8.png"),
+            Languages("Francés", "https://i.imgur.com/eLAmRHu.png"),
+            Languages("Alemán", "https://i.imgur.com/GNVTXZa.png"),
+            Languages("Japonés", "https://i.imgur.com/ITicXYZ.png"),
+            Languages("Italiano", "https://i.imgur.com/nrcoTdC.png"),
+            Languages("Portugués", "https://i.imgur.com/c3X82KG.png"),
         )
     }
 }
