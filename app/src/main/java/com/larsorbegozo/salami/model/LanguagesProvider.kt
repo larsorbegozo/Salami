f09417693a80beaa478cb6ad9661b69e8855327c
WezenKerrash <wezenkerrash44@gmail.com>
2022-09-08 19:56:09 -0300

    New links to new images

--- app/src/main/java/com/larsorbegozo/salami/model/LanguagesProvider.kt
+++ app/src/main/java/com/larsorbegozo/salami/model/LanguagesProvider.kt
@@ -3,12 +3,12 @@ package com.larsorbegozo.salami.model
 class LanguagesProvider {
     companion object {
         val languagesList: List<Languages> = listOf(
-            Languages("Inglés", "https://i.imgur.com/BOy4RJ8.png"),
-            Languages("Francés", "https://i.imgur.com/eLAmRHu.png"),
-            Languages("Alemán", "https://i.imgur.com/GNVTXZa.png"),
-            Languages("Japonés", "https://i.imgur.com/ITicXYZ.png"),
-            Languages("Italiano", "https://i.imgur.com/nrcoTdC.png"),
-            Languages("Portugués", "https://i.imgur.com/c3X82KG.png"),
+            Languages("Inglés", "https://i.imgur.com/ntzOWdw.png"),
+            Languages("Francés", "https://i.imgur.com/SVfUI8c.png"),
+            Languages("Alemán", "https://i.imgur.com/p8xJQz4.png"),
+            Languages("Japonés", "https://i.imgur.com/YTdzEN5.png"),
+            Languages("Italiano", "https://i.imgur.com/tY1vD1l.png"),
+            Languages("Portugués", "https://i.imgur.com/Fkts0xv.png"),
         )
     }
 }
